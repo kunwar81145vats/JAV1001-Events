326cea0f4da06d7fa7818c41824df946ecb9c1fc
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2022-02-24 06:00:39 +0530

    Commit details: 1. App name changed to 'Score Keeper'. 2. Comments added in code. 3. Bugs fixed.

--- app/src/main/java/com/example/layoutapp/MainActivity.java
+++ app/src/main/java/com/example/layoutapp/MainActivity.java
@@ -45,8 +45,14 @@ public class MainActivity extends AppCompatActivity {
         switchButtonAction();
     }
 
+    /*
+    Description: This method connects all UI elements to objects,
+    sets TextViews not editable by user,
+    selects default radio button for score type.
+     */
     void connectViews()
     {
+        //Connect UI elements
         teamOneTextView = findViewById(R.id.teamOneScoreText);
         teamTwoTextView = findViewById(R.id.teamTwoScoreText);
         increaseScoreButton = findViewById(R.id.increaseButton);
@@ -58,20 +64,30 @@ public class MainActivity extends AppCompatActivity {
         sixScoreRadio = findViewById(R.id.sixScoreRadio);
         teamSwitch = findViewById(R.id.teamSwitch);
 
+        //Disable user interaction in textviews
         teamOneTextView.setFocusable(false);
         teamTwoTextView.setFocusable(false);
+
+        //Select score 1 for default
         oneScoreRadio.setChecked(true);
     }
 
+    /*
+    Description: This method adds onClickListener to increase score button,
+    decrease score button and reset button
+     */
     void buttonActions()
     {
+        //increase button OnClickListener
         increaseScoreButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
+
+                //Check what score to increase by
                 switch (scoreValue) {
                     case 0:
                         int teamValue = 1;
 
+                        //Check which team is selected
                         if (teamSwitch.isChecked())
                         {
                             teamValue = teamValue + Integer.parseInt(teamTwoTextView.getText().toString());
@@ -87,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
                     case 1:
                         teamValue = 2;
 
+                        //Check which team is selected
                         if (teamSwitch.isChecked())
                         {
                             teamValue = teamValue + Integer.parseInt(teamTwoTextView.getText().toString());
@@ -101,6 +118,7 @@ public class MainActivity extends AppCompatActivity {
                     case 2:
                         teamValue = 4;
 
+                        //Check which team is selected
                         if (teamSwitch.isChecked())
                         {
                             teamValue = teamValue + Integer.parseInt(teamTwoTextView.getText().toString());
@@ -115,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
                     case 3:
                         teamValue = 6;
 
+                        //Check which team is selected
                         if (teamSwitch.isChecked())
                         {
                             teamValue = teamValue + Integer.parseInt(teamTwoTextView.getText().toString());
@@ -132,13 +151,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Decrease button OnClickListener
         decreaseScoreButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
+
+                //Check what score to decrease by
                 switch (scoreValue) {
                     case 0:
                         int scoreVal = 1;
 
+                        //Check which team is selected
                         if (teamSwitch.isChecked())
                         {
                             int teamScore = Integer.parseInt(teamTwoTextView.getText().toString());
@@ -146,11 +168,17 @@ public class MainActivity extends AppCompatActivity {
                             {
                                 teamScore = teamScore - scoreVal;
                             }
+                            else
+                            {
+                                Toast.makeText(activity, "Negative scores not allowed", Toast.LENGTH_LONG).show();
+                            }
                             teamTwoTextView.setText(String.valueOf(teamScore));
                         }
                         else
                         {
                             int teamScore = Integer.parseInt(teamOneTextView.getText().toString());
+
+                            //Condition to stop negative scores
                             if (teamScore >= scoreVal)
                             {
                                 teamScore = teamScore - scoreVal;
@@ -165,9 +193,12 @@ public class MainActivity extends AppCompatActivity {
                     case 1:
                         scoreVal = 2;
 
+                        //Check which team is selected
                         if (teamSwitch.isChecked())
                         {
                             int teamScore = Integer.parseInt(teamTwoTextView.getText().toString());
+
+                            //Condition to stop negative scores
                             if (teamScore >= scoreVal)
                             {
                                 teamScore = teamScore - scoreVal;
@@ -181,6 +212,8 @@ public class MainActivity extends AppCompatActivity {
                         else
                         {
                             int teamScore = Integer.parseInt(teamOneTextView.getText().toString());
+
+                            //Condition to stop negative scores
                             if (teamScore >= scoreVal)
                             {
                                 teamScore = teamScore - scoreVal;
@@ -195,9 +228,12 @@ public class MainActivity extends AppCompatActivity {
                     case 2:
                         scoreVal = 4;
 
+                        //Check which team is selected
                         if (teamSwitch.isChecked())
                         {
                             int teamScore = Integer.parseInt(teamTwoTextView.getText().toString());
+
+                            //Condition to stop negative scores
                             if (teamScore >= scoreVal)
                             {
                                 teamScore = teamScore - scoreVal;
@@ -211,6 +247,8 @@ public class MainActivity extends AppCompatActivity {
                         else
                         {
                             int teamScore = Integer.parseInt(teamOneTextView.getText().toString());
+
+                            //Condition to stop negative scores
                             if (teamScore >= scoreVal)
                             {
                                 teamScore = teamScore - scoreVal;
@@ -225,9 +263,12 @@ public class MainActivity extends AppCompatActivity {
                     case 3:
                         scoreVal = 6;
 
+                        //Check which team is selected
                         if (teamSwitch.isChecked())
                         {
                             int teamScore = Integer.parseInt(teamTwoTextView.getText().toString());
+
+                            //Condition to stop negative scores
                             if (teamScore >= scoreVal)
                             {
                                 teamScore = teamScore - scoreVal;
@@ -241,8 +282,16 @@ public class MainActivity extends AppCompatActivity {
                         else
                         {
                             int teamScore = Integer.parseInt(teamOneTextView.getText().toString());
+
+                            //Condition to stop negative scores
                             if (teamScore >= scoreVal)
+                            {
                                 teamScore = teamScore - scoreVal;
+                            }
+                            else
+                            {
+                                Toast.makeText(activity, "Negative scores not allowed", Toast.LENGTH_LONG).show();
+                            }
                             teamOneTextView.setText(String.valueOf(teamScore));
                         }
                         break;
@@ -252,6 +301,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Reset button OnClickListener
         resetScoreButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
@@ -262,6 +312,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //Description: This method adds OnClickListener to Switch button for team selection
     void switchButtonAction()
     {
         teamSwitch.setOnClickListener(new OnClickListener() {
@@ -269,19 +320,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (teamSwitch.isChecked()) {
+                    //Team 2 selected
                     teamSwitch.setText("Team 2");
                 } else {
+                    //Team 1 selected
                     teamSwitch.setText("Team 1");
                 }
             }
         });
-
-
-
-
     }
 
-
+    //Description: This method is called when any of the radio button is clicked
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
@@ -290,21 +339,25 @@ public class MainActivity extends AppCompatActivity {
         switch(view.getId()) {
             case R.id.OneScoreRadio:
                 if (checked) {
+                    //score 1 is selected
                     scoreValue = 0;
                 }
                 break;
             case R.id.twoScoreRadio:
                 if (checked) {
+                    //score 2 is selected
                     scoreValue = 1;
                 }
                 break;
             case R.id.fourScoreRadio:
                 if (checked) {
+                    //score 4 is selected
                     scoreValue = 2;
                 }
                 break;
             case R.id.sixScoreRadio:
                 if (checked) {
+                    //score 6 is selected
                     scoreValue = 3;
                 }
                 break;
